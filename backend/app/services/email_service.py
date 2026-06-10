d57ea99f3f5a99e51edee015d3b7fb3ec1fabcea
unknown <Zeel2506>
2026-06-11 00:10:53 +0530

    Error fixed

--- backend/app/services/email_service.py
+++ backend/app/services/email_service.py
@@ -1,17 +1,31 @@
 import os
-import resend
+import sib_api_v3_sdk
+from sib_api_v3_sdk.rest import ApiException
 
-print("RESEND KEY:", os.getenv("RESEND_API_KEY"))
-resend.api_key = os.getenv("RESEND_API_KEY")
+BREVO_API_KEY = os.getenv("BREVO_API_KEY")
 
-def send_email(
-    recipient: str,
-    subject: str,
-    body: str
-):
-    resend.Emails.send({
-        "from": "StudyGenie <onboarding@resend.dev>",
-        "to": recipient,
-        "subject": subject,
-        "text": body,
-    })
+def send_email(recipient, subject, body):
+    configuration = sib_api_v3_sdk.Configuration()
+    configuration.api_key['api-key'] = BREVO_API_KEY
+
+    api_instance = sib_api_v3_sdk.TransactionalEmailsApi(
+        sib_api_v3_sdk.ApiClient(configuration)
+    )
+
+    sender = {
+        "name": "StudyGenie",
+        "email": "your_verified_email@gmail.com"
+    }
+
+    send_smtp_email = sib_api_v3_sdk.SendSmtpEmail(
+        to=[{"email": recipient}],
+        sender=sender,
+        subject=subject,
+        text_content=body
+    )
+
+    try:
+        api_instance.send_transac_email(send_smtp_email)
+    except ApiException as e:
+        print("Brevo Error:", e)
+        raise
