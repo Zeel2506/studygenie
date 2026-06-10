b4c8218e6df81a24884468118abf511be7608002
unknown <Zeel2506>
2026-06-10 23:13:20 +0530

    email fix

--- backend/app/services/email_service.py
+++ backend/app/services/email_service.py
@@ -1,35 +1,16 @@
-import smtplib
-from email.mime.text import MIMEText
-from dotenv import load_dotenv
 import os
+import resend
 
-load_dotenv()
-
-EMAIL_USER = os.getenv("EMAIL_USER")
-EMAIL_PASSWORD = os.getenv("EMAIL_PASSWORD")
+resend.api_key = os.getenv("RESEND_API_KEY")
 
 def send_email(
     recipient: str,
     subject: str,
     body: str
 ):
-
-    msg = MIMEText(body)
-
-    msg["Subject"] = subject
-    msg["From"] = EMAIL_USER
-    msg["To"] = recipient
-
-    with smtplib.SMTP(
-        "smtp.gmail.com",
-        587
-    ) as server:
-
-        server.starttls()
-
-        server.login(
-            EMAIL_USER,
-            EMAIL_PASSWORD
-        )
-
-        server.send_message(msg)
+    resend.Emails.send({
+        "from": "StudyGenie <onboarding@resend.dev>",
+        "to": recipient,
+        "subject": subject,
+        "text": body,
+    })
